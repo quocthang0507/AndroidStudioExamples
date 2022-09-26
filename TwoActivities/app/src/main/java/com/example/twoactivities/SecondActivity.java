25a6e53a342b1070aa54a76f486e14a2b6773c28
La Quoc Thang <quocthang_0507@yahoo.com.vn>
2022-09-26 15:36:51 +0700

    Initial commit

---
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.example.twoactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    private TextView textView;
+    private EditText mReply;
+
+    public static final String EXTRA_REPLY = "com.example.twoactivities.extra.REPLY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        textView = findViewById(R.id.text_message);
+        mReply = findViewById(R.id.editText_second);
+        textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}
