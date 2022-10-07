eaf030ccda51cdccc3d18ed6437fb3a84979a200
La Quoc Thang <quocthang_0507@yahoo.com.vn>
2022-10-07 09:53:21 +0700

    Update project

--- HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.helloconstraint;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,12 +12,15 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private Button buttonCount, buttonZero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = findViewById(R.id.show_count);
+        buttonCount = findViewById(R.id.button_count);
+        buttonZero = findViewById(R.id.button_zero);
     }
 
     public void showToast(View view) {
@@ -26,15 +31,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         mCount++;
-        if (mShowCount != null) {
-            mShowCount.setText(Integer.toString(mCount));
+        mShowCount.setText(Integer.toString(mCount));
+        if (mCount % 2 == 0) {
+            buttonCount.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.gray)));
+        } else {
+            buttonCount.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.pink)));
         }
+        buttonZero.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.pink)));
     }
 
     public void reset(View view) {
         mCount = 0;
-        if (mShowCount != null) {
-            mShowCount.setText(Integer.toString(mCount));
-        }
+        mShowCount.setText(Integer.toString(mCount));
+        buttonZero.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.gray)));
     }
 }
